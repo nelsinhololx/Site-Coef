d4f2982792eea119e7a6f016174ca834dad1ca4b
Nelson Araujo de Oliveira <adonelson@hotmail.com>
2024-12-11 01:54:12 -0300

    Corrigindo erro na navbar e atualizando o layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
-const Page = () => {
+import Navbar from './components/Navbar';  // Supondo que a Navbar seja um componente separado
+
+export default function Home() {
   return (
     <div>
-      oiii
-      
+      <Navbar /> {/* Navbar que usa a imagem */}
+      <h1>Bem-vindo ao meu site!!!!!!</h1>
+      {/* Outros conteúdos da página */}
     </div>
   );
-};
-
-export default Page;
+}
