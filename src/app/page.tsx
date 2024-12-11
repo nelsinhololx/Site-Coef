6d47595bb3cb37c97b12f0e98076e1f1e20155c4
Nelson Araujo de Oliveira <adonelson@hotmail.com>
2024-12-11 01:30:28 -0300

    Atualizando o conteúdo da página inicial

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,13 +3,8 @@ import Image from 'next/image';
 const Page = () => {
   return (
     <div>
-      {/* Usando a imagem do Next.js */}
-      <Image
-        src="/logo-nav.png"  // O caminho começa com / para indicar a pasta public
-        alt="Logo de navegação"
-        width={200}  // Largura desejada
-        height={50}  // Altura desejada
-      />
+      oiii
+      
     </div>
   );
 };
